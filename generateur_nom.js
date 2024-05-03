4009c0adf0a0ca8800f8665cd2820ae9266d9cc0
ThomasHawwk <165882223+ThomasHawwk@users.noreply.github.com>
2024-05-03 08:34:26 +0200

    add function generate_personnage

--- generateur_nom.js
+++ generateur_nom.js
@@ -9,3 +9,24 @@ function genererNom() {
     // Retourner le nom choisi
     return noms[index];
 }
+
+// Fonction pour générer un nom de personnage aléatoire
+function genererNomPersonnage() {
+    const consonnes = "bcdfghjklmnpqrstvwxyz";
+    const voyelles = "aeiou";
+    const longueurNom = Math.floor(Math.random() * 3) + 3; // Entre 3 et 5 lettres
+
+    let nom = "";
+
+    // Ajoute des lettres au nom, en alternant consonnes et voyelles
+    for (let i = 0; i < longueurNom; i++) {
+        if (i % 2 === 0) {
+            nom += consonnes.charAt(Math.floor(Math.random() * consonnes.length));
+        } else {
+            nom += voyelles.charAt(Math.floor(Math.random() * voyelles.length));
+        }
+    }
+
+    // Retourne le nom généré
+    return nom.charAt(0).toUpperCase() + nom.slice(1);
+}
